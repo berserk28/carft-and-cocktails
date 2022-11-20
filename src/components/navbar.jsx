3c92c7b5f7879c50d6422821036ae6b934b7d38c
saker toufik <absolutefreeman@gmail.com>
2022-11-20 03:12:58 +0100

    cocktails

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -4,15 +4,7 @@ export default function Navbar() {
   return (
     <nav class="navbar ">
       <div className="container-fluid">
-        <div class="navbar-brand fs-2 fw-bold text-light">
-          <img
-            src="src/images/573c9be3db981bdb01103856ac26490e.webp"
-            width="30"
-            height="24"
-            class="d-inline-block align-text-top"
-          />
-          LIQUAL CLUB
-        </div>
+        <div class="navbar-brand fs-2 fw-bold text-light">sLIQUAL CLUB</div>
         <div className="d-flex">
           <div class="nav-item fs-4 m-2 me-3 ">
             <a class="nav-link px-3 py-2 rounded-pill" href="#">
