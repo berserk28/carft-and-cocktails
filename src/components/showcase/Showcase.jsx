97abe2cdd696ccccb15b5aac557e206ce88d5de2
saker toufik <absolutefreeman@gmail.com>
2022-12-07 22:19:45 +0100

    now

--- src/components/showcase/Showcase.jsx
+++ src/components/showcase/Showcase.jsx
@@ -1,5 +1,19 @@
 import React from "react";
 import "./showcase.css";
+
 export default function Showcase() {
-  return <div className="showcase-bg"></div>;
+  return (
+    <div className="showcase__container section__padding">
+      <div className="showcase__container-header">
+        <div className="showcase__container-header-title">
+          <h1 className="gradient-text">Cocktail Club</h1>
+          <p>The worlds best cocktail resource</p>
+        </div>
+        <div className="showcase-input">
+          <input type="text" />
+        </div>
+      </div>
+      <div className="showcase-image"></div>
+    </div>
+  );
 }
