a4843b2e1d4f152373164a47da90de020403bcac
saker toufik <absolutefreeman@gmail.com>
2022-12-10 23:24:14 +0100

    pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Navbar, Showcase, Footer } from "./components/index";
 import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./app.css";
 const App = () => {
   return (
