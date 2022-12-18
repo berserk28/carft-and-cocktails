9184e1f3f2833a60878c8f6742bffb80f6eaac2a
saker toufik <absolutefreeman@gmail.com>
2022-12-18 18:51:28 +0100

    query

--- src/components/List_cocktails/List_cocktails.jsx
+++ src/components/List_cocktails/List_cocktails.jsx
@@ -1,28 +1,41 @@
 import React from "react";
 import "./List_cocktails.css";
+import { useQuery } from "react-query";
 import { Search_bar } from "../index";
 import { Spinner } from "../index";
-import { useGlobalContext } from "../../context";
+
 function Cocktails() {
-  const { isLoading } = useGlobalContext();
-  if (isLoading)
-    return (
-      <div className="section__padding List_cocktails">
-        <div className="search-bar-container">
-          <Search_bar className="List_cocktails" />
-        </div>
-        <Spinner />
-      </div>
-    );
-  else {
-    return (
-      <div className="section__padding List_cocktails">
-        <div className="search-bar-container">
-          <Search_bar className="List_cocktails" />
-        </div>
-      </div>
-    );
-  }
+  const { isLoading, error, data } = useQuery("cocktail", () =>
+    fetch(
+      "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita/react-query"
+    ).then((res) => res.json())
+  );
+  console.log(error);
+  console.log(isLoading, data);
+  if (isLoading) return <h1>loading</h1>;
+
+  if (error) return <h1>error</h1>;
+
+  return data;
 }
 
 export default Cocktails;
+
+//  if (isLoading)
+//    return (
+//      <div className="section__padding List_cocktails">
+//        <div className="search-bar-container">
+//          <Search_bar className="List_cocktails" />
+//        </div>
+//        <Spinner />
+//      </div>
+//    );
+//  else {
+//    return (
+//      <div className="section__padding List_cocktails">
+//        <div className="search-bar-container">
+//          <Search_bar className="List_cocktails" />
+//        </div>
+//      </div>
+//    );
+//  }
--- src/index.js
+++ src/index.js
@@ -3,12 +3,18 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.css";
 import { AppProvider } from "./context";
+import { QueryClient, QueryClientProvider, useQuery } from "react-query";
 import App from "./App";
+import { ReactQueryDevtools } from "react-query/devtools";
+const queryClient = new QueryClient();
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <AppProvider>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+        <ReactQueryDevtools />
+      </QueryClientProvider>
     </AppProvider>
   </React.StrictMode>
 );
