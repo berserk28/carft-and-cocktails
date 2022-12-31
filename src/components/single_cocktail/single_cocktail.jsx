dffae2b33b78ebb41c023bd7fb4102d0371f562a
saker toufik <absolutefreeman@gmail.com>
2022-12-31 09:54:19 +0100

    f

--- src/components/single_cocktail/single_cocktail.jsx
+++ src/components/single_cocktail/single_cocktail.jsx
@@ -1,7 +1,11 @@
 import React from "react";
-
-const cocktail = () => {
-  return <div>cocktail</div>;
+import "./single_cocktail.css";
+const single_cocktail = ({ strAlcoholic, strDrinkThumb, strDrink }) => {
+  return (
+    <div className="single_cocktail">
+      <img src={strDrinkThumb} alt={strDrink} />
+    </div>
+  );
 };
 
-export default cocktail;
+export default single_cocktail;
