999628933c0cd29a6518f013ba989d2a57aac22f
saker toufik <absolutefreeman@gmail.com>
2023-01-01 02:33:51 +0100

    s

--- src/components/single_cocktail/single_cocktail.jsx
+++ src/components/single_cocktail/single_cocktail.jsx
@@ -1,11 +1,17 @@
 import React from "react";
+import { useState } from "react";
 import "./single_cocktail.css";
-const single_cocktail = ({ strAlcoholic, strDrinkThumb, strDrink }) => {
+const Single_cocktail = ({ strAlcoholic, strDrinkThumb, strDrink }) => {
+  const [IsActive, setIsActive] = useState(false);
+
   return (
-    <div className="single_cocktail">
+    <div className="single_cocktail-container">
       <img src={strDrinkThumb} alt={strDrink} />
+      <div className="single_cocktail-container-info">
+        <h3>{strDrink}</h3>
+      </div>
     </div>
   );
 };
 
-export default single_cocktail;
+export default Single_cocktail;
