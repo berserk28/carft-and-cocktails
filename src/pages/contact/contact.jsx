1b9e5671eca903b0e39d6ac9ef725391254d5c01
saker toufik <absolutefreeman@gmail.com>
2023-01-05 08:51:38 +0100

    go

--- src/pages/contact/contact.jsx
+++ src/pages/contact/contact.jsx
@@ -2,35 +2,26 @@ import React from "react";
 import "./contact.css";
 const contact = () => {
   return (
-    <div>
-      <div class="contact-form bg-green p1">
-        <h2 class="l-medium">Contact Us</h2>
+    <div className="container">
+      <div class="form  ">
+        <h2>Register With US</h2>
         <p>Please use the form below to contact us</p>
         <form>
-          <div class="form-group">
+          <div class="form-control">
             <label for="name">Name</label>
             <input type="text " id="name" placeholder="Name" />
           </div>
-          <div class="form-group">
+          <div class="form-control">
             <label for="email">Email</label>
             <input type="email " id="email" placeholder="Email" />
           </div>
-          <div class="form-group">
+          <div class="form-control">
             <label for="phone">Phone Number</label>
             <input type="number " id="phone" placeholder="Phone" />
           </div>
-          <div class="form-group">
-            <label for="message">Message</label>
-            <textarea
-              name="message"
-              id="message"
-              cols="50"
-              rows="2"
-              placeholder="Message"
-            ></textarea>
-          </div>
-          <div class="form-group">
-            <input type="submit" class="btn btn-dark" value="Send" />
+
+          <div class="form-control">
+            <button type="submit">Submit</button>
           </div>
         </form>
       </div>
