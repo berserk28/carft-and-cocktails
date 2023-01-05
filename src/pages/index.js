0ceca63227c6b643c9136c48755609079cf5a29b
saker toufik <absolutefreeman@gmail.com>
2023-01-05 15:21:35 +0100

    Updated export

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,3 +3,4 @@ export { default as About } from "./about/About";
 export { default as Contact } from "./contact/contact";
 export { default as Error } from "./error/error";
 export { default as SingleCocltail } from "./singleCocktail/SingleCoktail";
+console.log("");
