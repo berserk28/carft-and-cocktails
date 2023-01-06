f26b305a795d4bf228f3b358b11db20509350a0f
saker toufik <absolutefreeman@gmail.com>
2023-01-06 14:05:22 +0100

    Added component

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,4 +3,3 @@ export { default as About } from "./about/About";
 export { default as Contact } from "./contact/contact";
 export { default as Error } from "./error/error";
 export { default as SingleCocltail } from "./singleCocktail/SingleCoktail";
-console.log("");
