8f86a7a264f49936e47aff4b61bcb911c45ec0b7
saker toufik <absolutefreeman@gmail.com>
2023-01-07 19:08:44 +0100

    Added styling

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -2,17 +2,19 @@ import React from "react";
 import "./about.css";
 export default function About() {
   return (
-    <div className="about-container section__padding ">
-      <h1>
-        About <span>us</span>{" "}
-      </h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae
-        repudiandae architecto qui adipisci in officiis, aperiam sequi atque
-        perferendis eos, autem maiores nisi saepe quisquam hic odio consectetur
-        nobis veritatis quasi explicabo obcaecati doloremque? Placeat ratione
-        hic aspernatur error oiert
-      </p>
+    <div className="about-container ">
+      <div className="about-section">
+        <h1>
+          About <span>us</span>{" "}
+        </h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae
+          repudiandae architecto qui adipisci in officiis, aperiam sequi atque
+          perferendis eos, autem maiores nisi saepe quisquam hic odio
+          consectetur nobis veritatis quasi explicabo obcaecati doloremque?
+          Placeat ratione hic aspernatur error oiert
+        </p>
+      </div>
     </div>
   );
 }
