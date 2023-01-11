39dd329b13aacef0bf51d98bec7862e56134faad
saker toufik <absolutefreeman@gmail.com>
2023-01-11 19:14:48 +0100

    Added Styling to input

--- src/components/FormInput/FormInput.jsx
+++ src/components/FormInput/FormInput.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 
-const FormInput = () => {
-  return <div>FormInput</div>;
+const FormInput = (props) => {
+  const { label, placeholder, change, ...inputProps } = props;
+
+  return (
+    <div>
+      <label>{label}</label>
+      <input
+        {...inputProps}
+        type="text"
+        placeholder={placeholder}
+        onChange={change}
+      />
+    </div>
+  );
 };
 
 export default FormInput;
--- src/pages/contact/contact.jsx
+++ src/pages/contact/contact.jsx
@@ -3,50 +3,61 @@ import { useState } from "react";
 import "./contact.css";
 import FormInput from "../../components/FormInput/FormInput";
 const Contact = () => {
-  const [values, setValues] = useState({});
-  const Inputs = [
+  const [values, setValues] = useState({
+    Username: "",
+    Email: "",
+    date: "",
+    Password: "",
+    ComfirmPassword: "",
+  });
+
+  const inputs = [
     {
       id: 1,
       name: "Username",
       text: "text",
       label: "Username",
-      PlaceHolder: "Enter ur Username",
+      placeholder: "Enter ur Username",
     },
     {
       id: 2,
       name: "Email",
       text: "text",
       label: "Email",
-      PlaceHolder: "Enter ur Email",
+      placeholder: "Enter ur Email",
     },
     {
       id: 3,
       name: "date",
       text: "date",
       label: "date",
-      PlaceHolder: "Enter ur date",
+      placeholder: "Enter ur date",
     },
     {
       id: 4,
       name: "Password",
       text: "text",
       label: "Password",
-      PlaceHolder: "Enter ur Password",
+      placeholder: "Enter ur Password",
     },
     {
       id: 5,
-      name: "Comfirm Password",
+      name: "ComfirmPassword",
       text: "text",
-      label: "Comfirm Password",
-      PlaceHolder: "Enter ur Comfirm Password",
+      label: "ComfirmPassword",
+      placeholder: "Enter ur Comfirm Password",
     },
   ];
+  const onChange = (e) => {
+    setValues({ ...values, [e.target.name]: e.target.value });
+  };
+  console.log(values);
   return (
     <div className="form-container app-bg">
       <form className="form">
         <h2>register with us </h2>
-        {Inputs.map((item) => {
-          return <FormInput key={item.id} {...item} />;
+        {inputs.map((input) => {
+          return <FormInput key={input.id} {...input} change={onChange} />;
         })}
       </form>
     </div>
