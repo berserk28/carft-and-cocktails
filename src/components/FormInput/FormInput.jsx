09a9f2e8c1ff4f842cdeaa9d7c6290cfb46b4277
saker toufik <absolutefreeman@gmail.com>
2023-01-13 10:57:04 +0100

    Added functionality to validation form

--- src/components/FormInput/FormInput.jsx
+++ src/components/FormInput/FormInput.jsx
@@ -1,17 +1,29 @@
 import React from "react";
+import { useState } from "react";
 import "./FormInput.css";
 const FormInput = (props) => {
-  const { label, placeholder, change, ...inputProps } = props;
+  const { label, placeholder, change, errorMessage, text, ...inputProps } =
+    props;
+  const [focused, setFocused] = useState(false);
+  function focusHandler(e) {
+    setFocused(true);
+  }
 
   return (
     <div className="input-container">
       <label>{label}</label>
       <input
         {...inputProps}
-        type="text"
+        type={text}
         placeholder={placeholder}
         onChange={change}
+        onBlur={focusHandler}
+        focused={focused.toString()}
+        onFocus={() => {
+          inputProps.name === "ComfirmPassword" && setFocused(true);
+        }}
       />
+      {text !== "date" && <span>{errorMessage}</span>}
     </div>
   );
 };
--- src/pages/contact/contact.jsx
+++ src/pages/contact/contact.jsx
@@ -9,6 +9,7 @@ const Contact = () => {
     date: "",
     Password: "",
     ComfirmPassword: "",
+    phone: "",
   });
 
   const inputs = [
@@ -17,48 +18,73 @@ const Contact = () => {
       name: "Username",
       text: "text",
       label: "Username",
+      errorMessage:
+        "Username should be 3-16 characters and shouldn't include any special character !",
       placeholder: "Enter ur Username",
+      pattern: "^[A-Za-z0-9]{3,16}$",
+      required: true,
     },
     {
       id: 2,
       name: "Email",
-      text: "text",
+      text: "email",
+      errorMessage: "It should be a valid email address!",
       label: "Email",
       placeholder: "Enter ur Email",
+      required: true,
     },
     {
       id: 3,
-      name: "date",
-      text: "date",
-      label: "date",
-      placeholder: "Enter ur date",
+      name: "phone",
+      text: "text",
+      errorMessage: "Phone number should be at least 10 numbers !",
+      label: "Phone",
+      placeholder: "Enter ur phone number",
+      pattern: "^[0-9]{10}$",
+      required: true,
     },
     {
       id: 4,
-      name: "Password",
-      text: "text",
-      label: "Password",
-      placeholder: "Enter ur Password",
+      name: "date",
+      text: "date",
+      label: "date",
     },
     {
       id: 5,
+      name: "Password",
+      text: "password",
+      errorMessage:
+        "Password should be 8-20 characters and include at least 1 letter, 1 number and 1 special character !",
+      label: "Password",
+      placeholder: "Enter ur Password",
+      pattern: `^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,20}$`,
+      required: true,
+    },
+    {
+      id: 6,
       name: "ComfirmPassword",
-      text: "text",
-      label: "ComfirmPassword",
+      text: "password",
+      label: "Comfirm Password",
+      errorMessage: "Passwords don't match!",
       placeholder: "Enter ur Comfirm Password",
+      pattern: values.Password,
+      required: true,
     },
   ];
   const onChange = (e) => {
     setValues({ ...values, [e.target.name]: e.target.value });
   };
-  console.log(values);
+  const submitHandler = (e) => {
+    e.preventDefault();
+  };
   return (
-    <div className="form-container app-bg">
-      <form>
+    <div className="form-container">
+      <form className="form" onSubmit={submitHandler}>
         <h2>register with us </h2>
         {inputs.map((input) => {
           return <FormInput key={input.id} {...input} change={onChange} />;
         })}
+        <button className="form-btn">Submit</button>
       </form>
     </div>
   );
